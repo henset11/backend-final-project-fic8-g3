54ef29ddacfadd0099d2e1c2e7d72244b61487a0
henset11 <iputuhendrasetiawan@gmail.com>
2023-10-20 22:38:14 +0800

    changes

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -28,7 +28,7 @@
             </li>
 
             <li class="nav-item dropdown ">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Subjects</span></a>
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Mata Kuliah</span></a>
                 <ul class="dropdown-menu">
                     <li>
                         <a class="nav-link" href="#">Subject List</a>
